d1fb8d272666ed06b4654e280fd5c09d0c6b8c54
Derek Hsu <99681800+derekhsuu@users.noreply.github.com>
2022-09-20 17:20:54 -0700

    Update noncompliantcode.py

--- noncompliantcode.py
+++ noncompliantcode.py
@@ -6,3 +6,7 @@ def index(request):
     response["Set-Cookie"] = value  # Noncompliant
     response.set_cookie("sessionid", value)  # Noncompliant
     return response
+import crypt
+from hashlib import pbkdf2_hmac
+
+hash = pbkdf2_hmac('sha256', password, b'D8VxSmTZt2E2YV454mkqAY5e', 100000)    # Noncompliant: salt is hardcoded
